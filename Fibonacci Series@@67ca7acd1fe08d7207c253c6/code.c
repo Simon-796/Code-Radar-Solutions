cac5c125391f4d02bcdaacb4b034d8f774d99101
Simon <simonhambiria@gmail.com>
2025-03-21 23:05:09 +0530

    Solution to Fibonacci Series

--- Fibonacci Series@@67ca7acd1fe08d7207c253c6/code.c
+++ Fibonacci Series@@67ca7acd1fe08d7207c253c6/code.c
@@ -2,14 +2,14 @@
 int fibo(int a);
 int fibo(int a){
     if(a==1 || a==2){
-        return n-1;
+        return a-1;
     }
     return fibo(a-1)+fibo(a-2);
 }
 int main(){
     int a;
     scanf("%d", &a);
-    if(n<=0){
+    if(a<=0){
         printf("Invalid input\n");
     }
     int f = fibo(a);
