1d13f545ae986b694b55a9ab41d9d95b85fac818
Simon <simonhambiria@gmail.com>
2025-03-21 23:07:53 +0530

    Solution to Fibonacci Series

--- Fibonacci Series@@67ca7acd1fe08d7207c253c6/code.c
+++ Fibonacci Series@@67ca7acd1fe08d7207c253c6/code.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
-int fibo(int a);
-int fibo(int a){
+int fibonacciSeries(int a);
+int fibonacciSeries(int a){
     if(a==1 || a==2){
         return a-1;
     }
-    return fibo(a-1)+fibo(a-2);
+    return fibonacciSeries(a-1) + fibonacciSeries(a-2);
 }
 int main(){
     int a;
